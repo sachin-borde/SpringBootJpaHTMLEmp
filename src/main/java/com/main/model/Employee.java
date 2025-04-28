af96c0c56065a5804428a81bad1d7fd0afbc2693
sachin-borde <ssborde26@gmail.com>
2025-04-28 10:54:46 +0530

    message

---
+++ src/main/java/com/main/model/Employee.java
@@ -0,0 +1,54 @@
+package com.main.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotBlank(message = "Name is mandatory")
+	private String firstName;
+	@NotBlank(message = "Name is mandatory")
+	private String lastName;
+	@NotBlank(message = "Name is mandatory")
+	private String email;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
